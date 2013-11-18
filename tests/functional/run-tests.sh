bd53e1f83267dc7cc9aa9411c57a758935527bab
Kyle Meyer <kyle@kyleam.com>
2013-11-18 02:41:02 -0500

    Add functional tests

---
+++ tests/functional/run-tests.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+## Run functional tests
+
+## The input files are tracked by git, so any changes to their original
+## content will be shown
+
+inputdir=test-input
+files=($(ls "$inputdir"/file*txt))
+
+function check_gone (){
+    for f in ${files[@]}; do
+        [ -e $f ] && printf "$f exists but shouldn't\n" 1>&2
+    done
+}
+
+blind mask ${files[@]} > /dev/null
+printf "\nShould be four letter file names:\n"
+ls "$inputdir"/*.txt
+check_gone
+blind unmask "$inputdir"/blind-map-*.csv
+rm "$inputdir"/blind-map-*.csv
+
+blind mask -n ${files[@]} > /dev/null
+printf "\nShould be number files names:\n"
+ls "$inputdir"/*.txt
+check_gone
+blind unmask "$inputdir"/blind-map-*.csv
+rm "$inputdir"/blind-map-*.csv
+
+printf "\nShould fail with conflict error:\n"
+blind mask -n "$inputdir"/0 "$inputdir"/1
