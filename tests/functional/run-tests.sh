4465ac3b9db8eed39f1aa90a7dd714bf92a542b9
Kyle Meyer <kyle@kyleam.com>
2014-01-13 23:39:34 -0500

    Add git status check to functional tests
    - Don't proceed with test if working tree is dirty.
    - Provide message about whether passed.
    - Give non-zero exit codes on error.

--- tests/functional/run-tests.sh
+++ tests/functional/run-tests.sh
@@ -4,6 +4,11 @@
 ## The input files are tracked by git, so any changes to their original
 ## content will be shown.
 
+if [[ -n $(git status -s) ]]; then
+    printf "Working tree should be clean for tests.\n"
+    exit 1
+fi
+
 inputdir=test-input
 files=($(ls "$inputdir"/file*txt))
 
@@ -12,6 +17,8 @@ function check_gone (){
         [ -e $f ] && printf "$f exists but shouldn't\n" 1>&2
     done
 }
+
+blind -h > /dev/null || exit 1
 
 blind mask ${files[@]} > /dev/null
 printf "\nShould be four letter file names:\n"
@@ -29,3 +36,10 @@ rm "$inputdir"/blind-map-*.csv
 
 printf "\nShould fail with conflict error:\n"
 blind mask -n "$inputdir"/0 "$inputdir"/1
+
+if [[ -z $(git status -s) ]]; then
+    printf "\nNo file content changed. Check test output.\n"
+else
+    printf "\nSome tests failed.\n"
+    exit 2
+fi
